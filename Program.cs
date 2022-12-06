b134d688faf14def243a47c6a1af45928038f912
bulat-gab <gabdrakhmanov.br@gmail.com>
2022-12-06 21:15:26 +0700

    add day1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Collections.Generic;
+
+Console.WriteLine(Day1Part2());
+
+
+int Day1()
+{
+    int maxCalories = 0;
+    int currentElfCalories = 0;
+
+    foreach(var line in File.ReadAllLines("./input/day1"))
+    {
+        if (int.TryParse(line, out var calories))
+        {
+            currentElfCalories += calories;
+        }
+
+        if (string.IsNullOrWhiteSpace(line))
+        {
+            maxCalories = Math.Max(maxCalories, currentElfCalories);
+            currentElfCalories = 0;
+        }
+    }
+
+    return maxCalories;
+}
+
+int Day1Part2()
+{
+    var caloriesList = new List<int>();
+    int currentElfCalories = 0;
+
+    foreach (var line in File.ReadAllLines("./input/day1"))
+    {
+        if (int.TryParse(line, out var calories))
+        {
+            currentElfCalories += calories;
+        }
+
+        if (string.IsNullOrWhiteSpace(line))
+        {
+            caloriesList.Add(currentElfCalories);
+            currentElfCalories = 0;
+        }
+    }
+
+    return caloriesList.OrderByDescending(x => x).Take(3).Sum();
+}
